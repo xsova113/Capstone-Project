dd116572c4fb346756cfda135c4059763512a238
George <xsova113@gmail.com>
2023-02-16 20:57:50 -0800

    - Change filter button to picker style.

--- LittleLemon/Views/Components/FilterButton.swift
+++ LittleLemon/Views/Components/FilterButton.swift
@@ -9,11 +9,28 @@ import SwiftUI
 
 struct FilterButton: View {
     
+    @State private var selectedCategory: Category = Category.mains
+    
     var body: some View {
         HStack {
-            ForEach(Category.allCases, id: \.self) { category in
-                Button(category.rawValue) {
-                    
+//            ForEach(Category.allCases, id: \.self) { category in
+//                Button(category.rawValue) {
+//
+//                }
+//            }
+//            .padding(.horizontal)
+//            .padding(.vertical, 8)
+//            .background(Color(red: 0.93, green: 0.94, blue: 0.93, opacity: 1.00))
+//            .cornerRadius(15)
+//            .foregroundColor(Color(red: 0.29, green: 0.37, blue: 0.34, opacity: 1.00))
+//            .fontWeight(.heavy)
+//            .font(Font.system(size: 12))
+//            .padding(.horizontal, 5)
+            Text("Select Category: ")
+                .frame(width: 200, alignment: .leading)
+            Picker("Category", selection: $selectedCategory) {
+                ForEach(Category.allCases, id: \.self) { category in
+                    Text(category.rawValue)
                 }
             }
             .padding(.horizontal)
@@ -21,9 +38,8 @@ struct FilterButton: View {
             .background(Color(red: 0.93, green: 0.94, blue: 0.93, opacity: 1.00))
             .cornerRadius(15)
             .foregroundColor(Color(red: 0.29, green: 0.37, blue: 0.34, opacity: 1.00))
-            .fontWeight(.heavy)
-            .font(Font.system(size: 12))
-            .padding(.horizontal, 5)
+          
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
